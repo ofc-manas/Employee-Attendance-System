bafe64b5fa97fd7b9ca26eca4eb01786be52ae7c
Manas kashyap <manaskashyap389@gmail.com>
2026-09-02 15:14:47 +0530

    Update app.py

--- app.py
+++ app.py
@@ -10,10 +10,7 @@ app.secret_key = os.environ.get(
     "attendance-system-secret-key"
 )
 
-app.config["SQLALCHEMY_DATABASE_URI"] = os.environ.get(
-    "DATABASE_URL",
-    "sqlite:///attendance.db"
-)
+app.config["SQLALCHEMY_DATABASE_URI"] = os.environ.get("DATABASE_URL")
 
 app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
 
@@ -726,10 +723,6 @@ def logout():
     session.clear()
 
     return redirect(url_for("login"))
-
-
-with app.app_context():
-    db.create_all()
 
 if __name__ == "__main__":
     app.run(debug=True)
